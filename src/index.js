0ad347c2ebacb6a46eaa894bb5b49141c1a437a6
vmwhoami <vmwhoami@gmail.com>
2020-12-17 18:31:34 +0200

    add env variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import './styles/styles.scss';
 import inputValidtion from './assets/validations';
 import modal from './assets/modal';
-
-// require('dotenv').config()
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-
-// module.exports = {
-//   masterKey: process.env.KEY
-// };
-// let apikey = Key
-
-// console.log(apikey);
-
+let apikey = process.env.KEY
+console.log(apikey);
 // let input = document.getElementById("input")
 // let btn = document.getElementById("btn")
 // let info = document.querySelector('.location')
@@ -21,12 +10,11 @@ import modal from './assets/modal';
 
 
 // let forecast = (lat, lon, part = null) => {
-//   return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${process.env.API_KEY}`
+//   return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apikey}`
 // }
 
 // let weatherNow = (city) => {
-
-//   return `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}`
+//   return `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`
 // }
 
 
@@ -38,22 +26,25 @@ import modal from './assets/modal';
 //   }
 // })
 
+// let a = weatherNow("Chisinau")
+// console.log(a);
+
 // async function fetchData(url) {
 //   try {
 //     let response = await fetch(url);
 //     let weatherData = await response.json();
 
-//     // let city = weatherData.name
-//     // let county = weatherData.sys.country
-//     let lat = weatherData.coord.lat;
-//     let lon = weatherData.coord.lon;
-//     let forc = forecast(lat, lon, 'minutely,hourly')
-//     let predictions = await fetch(forc);
-//     let dayly = await predictions.json();
-//     console.log(dayly);
-//     // let next5days = await fetch(forecast);
+//     let city = weatherData.name
+    // let county = weatherData.sys.country
+    // let lat = weatherData.coord.lat;
+    // let lon = weatherData.coord.lon;
+    // let forc = forecast(lat, lon, 'minutely,hourly')
+    // let predictions = await fetch(forc);
+    // let dayly = await predictions.json();
+    // console.log(dayly);
+    // let next5days = await fetch(forecast);
 
-//     // console.log(next5days);
+    // console.log(next5days);
 
 
 //     info.textContent = city
