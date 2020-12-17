66a8904e8192e364e20bea2a55df528829fb0eb3
vmwhoami <vmwhoami@gmail.com>
2020-12-18 01:02:12 +0200

    add fahreinheit and celsius temp

--- src/assets/async.js
+++ src/assets/async.js
@@ -1,18 +1,39 @@
-let info = document.querySelector('.location')
-let countryInitials = document.querySelector('.countryInitials')
+
+let info = document.querySelector('.location');
+
+let getTemp = (data) => {
+  let num = Number(data.main.temp)
+  let celsius = num - 273.15
+  let fahrenheit = (num - 273.15) * 9 / 5 + 32
+  return [celsius, fahrenheit]
+}
+
+
+let populateDom = (data) => {
+  let location = document.querySelector('.location');
+  let countryInitials = document.querySelector('.countryInitials')
+  location.textContent = data.name
+  countryInitials.textContent = data.sys.country
+}
+
+let countryInitials = document.querySelector('.countryInitials');
 async function fetchData(url) {
   try {
     let response = await fetch(url);
-    let weatherData = await response.json();
-    let city = weatherData.name
-    let county = weatherData.sys.country
+    let data = await response.json();
+    populateDom(data)
+    let temp = getTemp(data)
+
+    console.log(temp);
     let lat = weatherData.coord.lat;
     let lon = weatherData.coord.lon;
-    console.log(lat);
+    console.log(weatherData);
     info.textContent = city
     countryInitials.textContent = county
   } catch (error) {
-    console.log(error);
+    let err = document.querySelector('.error__msg');
+    err.textContent = "Couldn't find your location"
+
   }
 
 }
--- src/assets/queries.js
+++ src/assets/queries.js
@@ -3,8 +3,8 @@ let apikey = process.env.KEY
 let forecast = (lat, lon, part = {}) => {
   return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apikey}`
 }
-let weatherNow = (city) => {
-  return `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`
+let weatherNow = (location) => {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apikey}`
 }
 
 export { forecast, weatherNow }
