b17da84f291212a740ad12e9435342d1557ef434
vmwhoami <vmwhoami@gmail.com>
2020-12-17 18:04:58 +0200

    add dotemv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -34,4 +35,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new Dotenv()],
 };
