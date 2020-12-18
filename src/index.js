d9e6752bc1a81c13c6611d83a253501cdb494bff
vmwhoami <vmwhoami@gmail.com>
2020-12-18 12:44:06 +0200

    add weather images

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 import './styles/styles.scss';
-import addEventsToSearch from './assets/events';
+import addEventsToSearch from './scripts/events';
 addEventsToSearch()
 
 
-let getTime = (str) => {
-  let time = str.split(',');
-  let hours = time[1].split(':');
-  let onlyH = `${hours[0]}:${hours[1]}`.trim()
-  return onlyH
-}
-
-function getLocalTime(offset) {
-  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-  let date = new Date();
-  let localTime = date.getTime();
-  let localOffset = date.getTimezoneOffset() * 60000;
-  let utc = localTime + localOffset;
-  localTime = utc + (1000 * offset);
-  let nd = new Date(localTime)
-  let day = days[nd.getDay()];
-  let dayOfMonth = nd.getDate()
-  let month = months[nd.getMonth()];
-  let time = nd.toLocaleString();
-  let onlyH = getTime(time);
-  return [day, dayOfMonth, month, onlyH]
-
-}
-
-let localTime = getLocalTime(10800);
-
-console.log(localTime);
